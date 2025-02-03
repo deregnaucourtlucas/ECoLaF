a40564ac7cd27f299a055cd6f87dc8ca413126ca
DeregnaucourtLucas <82165563+deregnaucourtlucas@users.noreply.github.com>
2025-02-03 16:18:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 
     name = 'ecolaf', 
     
-    version = '0.1.2',
+    version = '0.1.3',
     
     description = 'A toolkit python package to build an adaptive multimodal late fusion pipeline based on Dempster-Shafer Theory for semantic segmentation and classification tasks.',
     
