b3c593824a6bf7fdfdc4ece2ee188e373a2d0b1c
DeregnaucourtLucas <82165563+deregnaucourtlucas@users.noreply.github.com>
2025-02-13 16:14:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 
     name = 'ecolaf', 
     
-    version = '0.1.3',
+    version = '0.1.4',
     
     description = 'A toolkit python package to build an adaptive multimodal late fusion pipeline based on Dempster-Shafer Theory for semantic segmentation and classification tasks.',
     
@@ -28,7 +28,7 @@ setup(
 
     install_requires = [
 
-        'torch ~= 2.0.0'
+        'torch'
 
     ],
     
